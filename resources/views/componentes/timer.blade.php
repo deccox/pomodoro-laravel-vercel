c4631296f4e827494e5d9325c825e8eaa7f54ea0
deccox <josekayna13@gmail.com>
2024-09-13 00:10:08 -0300

    style

---
+++ resources/views/componentes/timer.blade.php
@@ -0,0 +1,56 @@
+<div id="left">
+    <div class="streak">
+        <div class="streakimg">
+            <img src="{{ asset('img/fire.svg') }}" alt="">
+            <span>123</span>
+        </div>
+        <div class="streaktoday" >
+            <div class="quads" >
+            </div>
+            <div class="quads" >
+            </div>
+            <div class="quads" >
+            </div>
+            <div class="quads" >
+            </div>
+            <div class="quads" >
+            </div>
+            <div class="quads" >
+            </div>
+            <div class="quads" >
+            </div>
+            <div class="quads" >
+            </div>
+            <div class="quads" >
+            </div>
+        </div>
+    </div>
+    <div class="board-img flex">
+        <img src="{{ asset('img/cat1.svg') }}" alt="">
+    </div>
+    <div class="board-timer flex">
+        <div class="borderbox"></div>
+        <div class="borderbox"></div>
+    </div>
+    <div class="board-buttons">
+        <button>START/STOP</button>
+        <button>
+            <img src="{{ asset('img/gear.svg') }}" alt="">
+        </button>
+    </div>
+</div>
+
+<div id="left">
+    <div class="flex">
+1
+    </div>
+    <div class="flex">
+
+    </div>
+    <div class="flex">
+
+    </div>
+    <div class="flex">
+
+    </div>
+</div>
