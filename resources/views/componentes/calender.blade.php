90402cbd2abbf869e01d035c58a7d5803d7b0f02
deccox <josekayna13@gmail.com>
2024-09-13 14:47:18 -0300

    feat: body calender

---
+++ resources/views/componentes/calender.blade.php
@@ -0,0 +1,57 @@
+<div id="rigth">
+    <div id="calender">
+        <div class="calendertitle">
+            Setembro 09
+        </div>
+        <div class="calendertable">
+
+        </div>
+    </div>
+    <div class="any">
+
+    </div>
+</div>
+
+
+@push('calenderscripts')
+
+<script>
+
+    $(document).ready(function() {
+        let week = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'];
+        let table = $('<table></table>').addClass('table_calender');
+        let tbody = $('<tbody></tbody>')
+        let thread = $('<thead></thead>');
+        let headerrow = $('<tr></tr>');
+        let bodyrow = $('<tr></tr>')
+
+        week.forEach(item => {
+            headerrow.append($('<th></th>').text(item));
+        })
+
+
+        for(let i = 0; i< 35; i++){
+            
+            if ( i % 7 == 0 && i !== 0){
+                tbody.append(bodyrow)
+                bodyrow = $('<tr></tr>')
+            }
+            bodyrow.append($('<td></td>').attr('data-data', i).append($('<span>'+i+'</span>')))
+
+        }
+
+
+        thread.append(headerrow);
+        tbody.append(bodyrow)
+        table.append(thread);
+        table.append(tbody);
+
+        $('.calendertable').append(table); // Correção do seletor
+
+        console.log(table);
+    });
+
+
+</script>
+
+@endpush
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/calender.css') }}">
     <script src="https://kit.fontawesome.com/db387ad2ac.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <title>Pomodoro</title>
@@ -16,5 +17,6 @@
 
 
     @stack('timerscripts')
+    @stack('calenderscripts')
 </body>
 </html>
